3b5431066521f0a8768a80ca18ae64a803107e27
TurburatoR <98270876+TurburatoR@users.noreply.github.com>
2022-01-23 21:58:27 +0700

    Update database2.inc.php
    Add Tuya roller motor

--- modules/zigbee2mqtt/database2.inc.php
+++ modules/zigbee2mqtt/database2.inc.php
@@ -11125,8 +11125,42 @@ $par2['xy'] = "";
 $par2['command_value'] = "state_right";
 SQLInsert('zigbee2mqtt_devices_command', $par2);
 
+//TS0601;
+$par2['zigbeeModel'] = 'TS0601';
+$par2['type'] = "cover";
+$par2['state_topic'] =  "<Z2M_PATH>/<FRIENDLY_NAME>";
+$par2['availability_topic'] = "<Z2M_PATH>/bridge/state";
+$par2['payload_off'] = ""  ;
+$par2['payload_on'] = ""  ;
+$par2['value_template'] = "position";
+$par2['command_topic'] = "<Z2M_PATH>/<FRIENDLY_NAME>/set"  ;
+$par2['json_attributes'] = "";
+$par2['device_class'] = "";
+$par2['unit_of_measurement'] = "";
+$par2['brightness'] = "";
+$par2['color_temp'] = "";
+$par2['xy'] = "";
+$par2['command_value'] = "position";
+SQLInsert('zigbee2mqtt_devices_command', $par2);
 
 
+//TS0601;
+$par2['zigbeeModel'] = 'TS0601';
+$par2['type'] = "cover";
+$par2['state_topic'] =  "<Z2M_PATH>/<FRIENDLY_NAME>";
+$par2['availability_topic'] = "<Z2M_PATH>/bridge/state";
+$par2['payload_off'] = "CLOSE"  ;
+$par2['payload_on'] = "OPEN"  ;
+$par2['value_template'] = "state";
+$par2['command_topic'] = "<Z2M_PATH>/<FRIENDLY_NAME>/set"  ;
+$par2['json_attributes'] = "";
+$par2['device_class'] = "";
+$par2['unit_of_measurement'] = "";
+$par2['brightness'] = "";
+$par2['color_temp'] = "";
+$par2['xy'] = "";
+$par2['command_value'] = "state";
+SQLInsert('zigbee2mqtt_devices_command', $par2);
 
 }
 
